aa4c081b0371fb610988b26a14752e9d5476698d
Mike Daly <github.com@metamike.net>
2012-04-18 22:45:35 -0700

    *entry.xml  -Moved buttons into their own   LinearLayout view  -Wrapped TableLayout with a   ScrollView  -Created a RelativeLayout to act   as a root view and anchored the   button view to its bottom

--- test/src/net/metamike/paymentreminder/test/EntryActivityTest.java
+++ test/src/net/metamike/paymentreminder/test/EntryActivityTest.java
@@ -1,9 +1,12 @@
 package net.metamike.paymentreminder.test;
 
 import net.metamike.paymentreminder.EntryActivity;
+import android.graphics.Rect;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 
@@ -24,14 +27,14 @@ public class EntryActivityTest extends ActivityInstrumentationTestCase2<EntryAct
 	}
 	
 	public void testPreconditions() {
-		assertNotNull("Entry view is null", activity.findViewById(net.metamike.paymentreminder.R.id.entryView));
+		assertNotNull("Root view is null", activity.findViewById(net.metamike.paymentreminder.R.id.root));
 		
 	}
 
 	public void testLayout() {
-		TableLayout entryView = (TableLayout) activity.findViewById(net.metamike.paymentreminder.R.id.entryView);
-		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.button_cancel));
-		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.button_save));
+		View mainView = activity.findViewById(net.metamike.paymentreminder.R.id.root);
+		TableLayout entryView = (TableLayout) mainView.findViewById(net.metamike.paymentreminder.R.id.entryView);
+		assertNotNull(entryView);
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_account));
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_amount_due));
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_amount_paid));
@@ -40,5 +43,23 @@ public class EntryActivityTest extends ActivityInstrumentationTestCase2<EntryAct
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_pay_reminder));
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_transfer_date));
 		assertNotNull(entryView.findViewById(net.metamike.paymentreminder.R.id.field_transfer_reminder));
+		
+
+		LinearLayout buttonsView = (LinearLayout)mainView.findViewById(net.metamike.paymentreminder.R.id.buttonsView);
+		assertNotNull(buttonsView);
+		assertNotNull(buttonsView.findViewById(net.metamike.paymentreminder.R.id.button_cancel));
+		assertNotNull(buttonsView.findViewById(net.metamike.paymentreminder.R.id.button_save));
+		
+		/* Test that button is visible on the screen */
+		Button cancel = (Button)activity.findViewById(net.metamike.paymentreminder.R.id.button_cancel);
+
+		int[] mainLocation = new int[2];
+		mainView.getLocationOnScreen(mainLocation);
+		int[] cancelLocation = new int[2];
+		cancel.getLocationOnScreen(cancelLocation);
+		
+		assertTrue(cancelLocation[0] + cancel.getWidth() <= mainLocation[0] + mainView.getWidth());
+		assertTrue(cancelLocation[1] + cancel.getHeight() <= mainLocation[1] + mainView.getHeight());
+		
 	}
 }
